8c3d91e6e9f7fda21143f6f4f0f95a0749749e76
Daniel <hacking61@hotmail.com>
2013-05-31 10:18:39 -0500

    Se sube la base para un proyecto de Ruby On Rails.

---
+++ app/controllers/principal_controller.rb
@@ -0,0 +1,11 @@
+class PrincipalController < ApplicationController
+def index
+     mensaje = "Hola a todos...<br>"
+     ["Pedro", "Pablo", "Juan"].each do |nombre|
+       mensaje += "y a "+nombre+" tambien<br>"
+     end
+     render :text => mensaje
+     puts 'Hola'
+   end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Direcciones::Application.config.secret_token = 'ff0d72c9d278b6ffe880e6d3f2ca7b0bf037065011622291062f038ecb2aa697aab9657860f817ec84be3f6e5708fc11ef7c4e789b3890f03f675ba0b3cfe31b'
