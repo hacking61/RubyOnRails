227170b1e63e1ffe0b7ee99660643864462827fb
Daniel <hacking61@hotmail.com>
2013-05-31 10:30:09 -0500

    SE AGREGAN COMENTARIOS AL ARCHIVO ROUTE.RB ==> ARCHIVO DONDE SE DECLARAN LOS CONTROLLERS CREADOS

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Direcciones::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
     #match '/' => 'principal#index'
+    #EDITADO X DAVO, ESTE ES EL CONTROLLER DECLARADO: EL CONTROLLER SE CREO CON EL SIGUIENTE COMANDO:
+    #rails generate controller principal
     match 'prueba' => 'principal#index', :as => 'index'
 
   # Sample of regular route:
@@ -54,7 +56,7 @@ Direcciones::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  #EDITADO POR DVO, ESTE ES LA PAGINA PRINCIPAL QUE CARGARÁ POR DEFAULT
+  #EDITADO POR DVO, ESTE ES LA PAGINA PRINCIPAL QUE CARGARï¿½ POR DEFAULT
   #root :to => 'principal#index'
   
 
